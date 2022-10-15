dfbc9bd2eba378c1d7fe10ec1a4486121cc2d966
riyas2516 <danishlion46@gmail.com>
2022-10-14 21:40:41 -0700

    dart programs

---
+++ class.dart
@@ -0,0 +1,9 @@
+void main() {
+  Car c = new Car('E1001');
+}
+
+class Car {
+  Car(String engine) {
+    print(engine);
+  }
+}
---
+++ sets.dart
@@ -0,0 +1,6 @@
+void main() {
+  var name = <String>{'riyaskhan'};
+  print("Output of first set: $name");
+  Set<String> age = {'23'};
+  print("Output of second set: age");
+}
